34401087ffef0ca4fee1cb4efe99e0c90c148da5
HedioKojima <46861125+HedioKojima@users.noreply.github.com>
2026-04-25 20:39:00 +0800

    Update mpv.cmake

--- packages/mpv.cmake
+++ packages/mpv.cmake
@@ -12,7 +12,7 @@ ExternalProject_Add(mpv
         spirv-cross
         zlib
         subrandr
-    GIT_REPOSITORY https://github.com/Kosm8/mpv.git
+    GIT_REPOSITORY https://github.com/mpv-player/mpv.git
     SOURCE_DIR ${SOURCE_LOCATION}
     GIT_CLONE_FLAGS "--filter=tree:0"
     UPDATE_COMMAND ""
